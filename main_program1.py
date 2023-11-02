3827ac15d5eb94e76a7362e0873fedff517468a8
ikacahyaagustina <149259465+ikacahyaagustina@users.noreply.github.com>
2023-11-02 18:29:37 +0700

    Update main_program1.py

--- main_program1.py
+++ main_program1.py
@@ -2,10 +2,10 @@ from cara_order1 import order
 
 print("Selamat Datang Di Pizza Hut")
 
-#Menampilkan fungsi order
-hasil_order = order()
+#Menampilkan cara_order1
+pesanan_pizza = order()
 
-#Menampilkan harga total
-print(f"Total harga dari pesanan pizza anda adalah Rp {(hasil_order)}")
+#Menampilkan total harga
+print(f"Total harga dari pesanan pizza anda adalah Rp {(pesanan_pizza)}")
 
 print("Terima Kasih Telah Memakai Layanan Kami")
