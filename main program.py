85bacd9f7e4b7131ac4dd7757481b042c4d728a2
ikacahyaagustina <149259465+ikacahyaagustina@users.noreply.github.com>
2023-11-02 16:32:37 +0700

    Update main program.py

--- main program.py
+++ main program.py
@@ -1,18 +1,11 @@
 from fungsi_order import order
 
-print("""
---------------Selamat Datang Di Pizza Hut--------------
-""")
+print("Selamat Datang Di Pizza Hut")
 
 #Menampilkan fungsi order
 hasil_order = order()
 
 #Menampilkan harga total
-print(f"""
-Total harga dari pesanan pizza anda adalah Rp (hasil_order),00
-""")
+print(f"Total harga dari pesanan pizza anda adalah Rp (hasil_order),00")
 
-#Menampilkan variasi pizza(opsional)
-print("""
---------------Terima Kasih Telah Memakai Layanan Kami--------------
-""")
+print("Terima Kasih Telah Memakai Layanan Kami")
